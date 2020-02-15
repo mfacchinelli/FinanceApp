989465ab6728b66408e2ecb63dea6e20d619554e
mfacchinelli <m.facchinelli@student.tudelft.nl>
2020-02-15 10:50:24 +0000

    Replaced custom function with built-in

--- functions/getRootFigure.m
+++ functions/getRootFigure.m
@@ -5,15 +5,7 @@ function obj = getRootFigure(obj)
 % Check input.
 narginchk(1, 1)
 
-% Iterate over parents until reaching with a figure object.
-while ~isa(obj, "matlab.ui.Figure")
-    % Retireve parent.
-    obj = obj.Parent;
-    
-    % Check that graphics root is not reached.
-    if isa(obj, "matlab.ui.Root") || isa(obj, "matlab.graphics.GraphicsPlaceholder")
-        error("MATLAB:getRootFigure:FigureNotFound", "No figure was found.")
-    end
-end
+% Call built-in ancestor function.
+obj = ancestor(obj, 'figure', 'toplevel');
 
 end
